a5c77065481a0582639479c9634675265ee5aa4c
sakumoto <mochorizo@gmail.com>
2023-05-29 15:07:43 +0000

    validate追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def create
     #binding.pry
-    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user = User.new(user_params)
     if @user.save
       flash[:info] = '登録が完了しました'
       redirect_to root_path
@@ -13,5 +13,10 @@ class UsersController < ApplicationController
       flash.now[:danger] = '登録に失敗しました'
       render :new
     end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { minimum: 1, maximum: 15 }
+  
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)(?=.*?[\W_])[!-~]{8,}+\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 8, maximum: 24 }
+  
+  has_secure_password
 end
