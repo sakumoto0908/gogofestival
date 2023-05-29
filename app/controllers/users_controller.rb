e84e932bc40d34ee45b7266ed6b7e95f695e1d8c
sakumoto <mochorizo@gmail.com>
2023-05-29 13:38:11 +0000

    flash機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
     #binding.pry
     @user = User.new(name: params[:user][:name], email: params[:user][:email])
     if @user.save
+      flash[:info] = '登録が完了しました'
       redirect_to root_path
     else
+      flash.now[:danger] = '登録に失敗しました'
       render :new
     end
   end
