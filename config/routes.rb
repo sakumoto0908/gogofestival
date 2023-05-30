5c872056fa6924ed5baea45d0e5cd22c666b585a
sakumoto <mochorizo@gmail.com>
2023-05-30 15:18:37 +0000

    ログイン・ログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  
   resources :users
+
+  get '/login',  to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  delete '/login',  to: 'sessions#destroy'
 end
